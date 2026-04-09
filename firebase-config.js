cd17abe7a4bcc28741be63ddd75e11ec8ee60d5d
rajahaider50 <rajahaider9053@gmail.com>
2026-04-09 19:56:19 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+/**
+ * Homigo Care - Firebase Configuration
+ * This file exports the configuration object required to initialize
+ * Firebase services across the application.
+ */
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCrcPI8ReNNKhsi6Kcn5Qeux4sgl5adp7E",
+  authDomain: "homigo-care-8360e.firebaseapp.com",
+  projectId: "homigo-care-8360e",
+  storageBucket: "homigo-care-8360e.firebasestorage.app",
+  messagingSenderId: "90367474956",
+  appId: "1:90367474956:web:bd378ece175338e5729d29",
+  measurementId: "G-8HJ3JY45FT"
+};
